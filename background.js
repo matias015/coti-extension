c70a5c6cc3b7ebb9f958b4b4808b7cfb23544b40
matias015 <78274208+matias015@users.noreply.github.com>
2021-09-27 02:19:17 -0300

    2.0v

--- background.js
+++ background.js
@@ -1,3 +1,9 @@
-chrome.browserAction.onClicked.addListener(function (tab) {
-  chrome.tabs.executeScript(null, { file: "main.js" });
+//chrome.browserAction.onClicked.addListener(function (tab) {
+//chrome.tabs.executeScript(null, { file: "main.js" });
+//});
+
+chrome.tabs.onUpdated.addListener(function (tabId, info, tab) {
+  if (info.status == "complete") {
+    chrome.tabs.executeScript(null, { file: "main.js" });
+  }
 });
