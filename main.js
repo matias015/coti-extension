5c78ee1c51718e43ed16173ea181249cdf5507df
matias015 <78274208+matias015@users.noreply.github.com>
2021-10-08 22:22:37 -0300

    2.1v
    autodetect if its already initialized

--- main.js
+++ main.js
@@ -1,94 +1,93 @@
-// funciones y vars
+if (!document.getElementById("showbox")) {
+  function Main() {
+    //func para extrar datos del prod y calc precio final
+    const calc_product_price = (e, element) => {
+      //definir variables para luego compleatar los campos en pricebox
+      let item_name;
+      let cost_price;
+      let pric_35;
+      let price_40;
+      let price_35_uni;
+      let price_40_uni;
+      let product_id;
+      let units;
 
-function Main() {
-  //func para extrar datos del prod y calc precio final
-  const calc_product_price = (e, element) => {
-    //definir variables para luego compleatar los campos en pricebox
-    let item_name;
-    let cost_price;
-    let pric_35;
-    let price_40;
-    let price_35_uni;
-    let price_40_uni;
-    let product_id;
-    let units;
+      let selected_product;
+      if (element) {
+        selected_product = element.querySelector(".producto");
+      } else {
+        selected_product = e.target.parentElement.querySelector(".producto");
+      }
 
-    let selected_product;
-    if (element) {
-      selected_product = element.querySelector(".producto");
-    } else {
-      selected_product = e.target.parentElement.querySelector(".producto");
-    }
+      //precio costo
+      cost_price = selected_product.querySelector(".producto_precio");
+      cost_price = cost_price.textContent.replaceAll("$", "");
+      cost_price = parseFloat(cost_price.trim());
 
-    //precio costo
-    cost_price = selected_product.querySelector(".producto_precio");
-    cost_price = cost_price.textContent.replaceAll("$", "");
-    cost_price = parseFloat(cost_price.trim());
+      //nombre producto
+      item_name = selected_product.querySelector(".producto_txt").textContent;
 
-    //nombre producto
-    item_name = selected_product.querySelector(".producto_txt").textContent;
+      //conseguir codigo
+      product_id = selected_product.querySelector(".producto_id").textContent;
 
-    //conseguir codigo
-    product_id = selected_product.querySelector(".producto_id").textContent;
+      product_id = parseInt(product_id.replaceAll("Código: ", ""));
 
-    product_id = parseInt(product_id.replaceAll("Código: ", ""));
+      //conseguir el input de unidades
+      units = parseFloat(
+        selected_product.parentElement.querySelector(".cant--input").value
+      );
 
-    //conseguir el input de unidades
-    units = parseFloat(
-      selected_product.parentElement.querySelector(".cant--input").value
-    );
+      //si es 0 o esta vacio o hay una letra, sera 1
+      if (!units) units = 1;
 
-    //si es 0 o esta vacio o hay una letra, sera 1
-    if (!units) units = 1;
+      //calcular precios al 35 y al 40 con las unidades
+      price_35 = Math.round(cost_price * 1.05 * 1.21 * 1.35 * 100) / 100;
+      price_35 = Math.round(price_35 * 4) / 4;
 
-    //calcular precios al 35 y al 40 con las unidades
-    price_35 = Math.round(cost_price * 1.05 * 1.21 * 1.35 * 100) / 100;
-    price_35 = Math.round(price_35 * 4) / 4;
+      price_40 = Math.round(cost_price * 1.05 * 1.21 * 1.4 * 100) / 100;
+      price_40 = Math.round(price_40 * 4) / 4;
 
-    price_40 = Math.round(cost_price * 1.05 * 1.21 * 1.4 * 100) / 100;
-    price_40 = Math.round(price_40 * 4) / 4;
+      price_35_uni = Math.round((price_35 / units) * 100) / 100;
+      price_35_uni = Math.round(price_35_uni * 4) / 4;
 
-    price_35_uni = Math.round((price_35 / units) * 100) / 100;
-    price_35_uni = Math.round(price_35_uni * 4) / 4;
+      price_40_uni = Math.round((price_40 / units) * 100) / 100;
+      price_40_uni = Math.round(price_40_uni * 4) / 4;
 
-    price_40_uni = Math.round((price_40 / units) * 100) / 100;
-    price_40_uni = Math.round(price_40_uni * 4) / 4;
+      //mostrar los resultados en la box
+      $box_item_name.textContent = item_name;
+      $box_item_units.textContent = units;
+      $box_cost_price.textContent = cost_price;
+      $box_price_35.textContent = price_35;
+      $box_price_40.textContent = price_40;
+      $box_price_35_uni.textContent = price_35_uni;
+      $box_price_40_uni.textContent = price_40_uni;
+      $box_product_id.textContent = product_id;
 
-    //mostrar los resultados en la box
-    $box_item_name.textContent = item_name;
-    $box_item_units.textContent = units;
-    $box_cost_price.textContent = cost_price;
-    $box_price_35.textContent = price_35;
-    $box_price_40.textContent = price_40;
-    $box_price_35_uni.textContent = price_35_uni;
-    $box_price_40_uni.textContent = price_40_uni;
-    $box_product_id.textContent = product_id;
+      bg_color_select(units);
+    };
 
-    bg_color_select(units);
-  };
+    const bg_color_select = (units) => {
+      //resaltar un precio u otro dependiendo de las unidades
+      if (units == 1) {
+        $box_price_35.style.backgroundColor = "transparent";
+        $box_price_40.style.backgroundColor = "black";
+        $box_price_35_uni.style.backgroundColor = "transparent";
+        $box_price_40_uni.style.backgroundColor = "transparent";
+      } else {
+        $box_price_35.style.backgroundColor = "black";
+        $box_price_40.style.backgroundColor = "transparent";
+        $box_price_35_uni.style.backgroundColor = "transparent";
+        $box_price_40_uni.style.backgroundColor = "black";
+      }
+    };
 
-  const bg_color_select = (units) => {
-    //resaltar un precio u otro dependiendo de las unidades
-    if (units == 1) {
-      $box_price_35.style.backgroundColor = "transparent";
-      $box_price_40.style.backgroundColor = "black";
-      $box_price_35_uni.style.backgroundColor = "transparent";
-      $box_price_40_uni.style.backgroundColor = "transparent";
-    } else {
-      $box_price_35.style.backgroundColor = "black";
-      $box_price_40.style.backgroundColor = "transparent";
-      $box_price_35_uni.style.backgroundColor = "transparent";
-      $box_price_40_uni.style.backgroundColor = "black";
-    }
-  };
+    /////////////////////////////////////////////////////////////////////////////////////////////////////
 
-  /////////////////////////////////////////////////////////////////////////////////////////////////////
+    // MAIN
 
-  // MAIN
-
-  //caja con precios
-  let $show_prices_box = document.createElement("div");
-  $show_prices_box.innerHTML = `<div id="showbox" class="box">
+    //caja con precios
+    let $show_prices_box = document.createElement("div");
+    $show_prices_box.innerHTML = `<div id="showbox" class="box">
   <p>
       <span class="item" contenteditable id="item"></span>
   </p>
@@ -207,105 +206,108 @@ function Main() {
 
 </style>`;
 
-  //agregar caja a dom
-  document.body.appendChild($show_prices_box);
+    //agregar caja a dom
+    document.body.appendChild($show_prices_box);
 
-  //objeto con elementos contenedores de los precios finales en la box
-  let $box_container = document.getElementById("showbox");
-  let $box_item_name = document.getElementById("item");
-  let $box_item_units = document.getElementById("unidades");
-  let $box_cost_price = document.getElementById("precioCosto");
-  let $box_price_35 = document.getElementById("precio35");
-  let $box_price_40 = document.getElementById("precio40");
-  let $box_price_35_uni = document.getElementById("precio35Uni");
-  let $box_price_40_uni = document.getElementById("precio40Uni");
-  let $box_product_id = document.getElementById("codigo");
+    //objeto con elementos contenedores de los precios finales en la box
+    let $box_container = document.getElementById("showbox");
+    let $box_item_name = document.getElementById("item");
+    let $box_item_units = document.getElementById("unidades");
+    let $box_cost_price = document.getElementById("precioCosto");
+    let $box_price_35 = document.getElementById("precio35");
+    let $box_price_40 = document.getElementById("precio40");
+    let $box_price_35_uni = document.getElementById("precio35Uni");
+    let $box_price_40_uni = document.getElementById("precio40Uni");
+    let $box_product_id = document.getElementById("codigo");
 
-  //estilo de boton y input
-  let calc_button_style =
-    "cursor:pointer;border-radius:0.5em;text-align:center;width:60%;background-color:green;color:#fff;padding:0.5em;margin:1.5em auto 0.5em auto;";
+    //estilo de boton y input
+    let calc_button_style =
+      "cursor:pointer;border-radius:0.5em;text-align:center;width:60%;background-color:green;color:#fff;padding:0.5em;margin:1.5em auto 0.5em auto;";
 
-  let calc_input_style =
-    "border-radius:0.5em;text-align:center;width:90%;color:#000;padding:0.25em;margin:0 auto 2em auto;";
+    let calc_input_style =
+      "border-radius:0.5em;text-align:center;width:90%;color:#000;padding:0.25em;margin:0 auto 2em auto;";
 
-  //cada elemento de la pag
-  const $on_page_products = document.querySelectorAll(".producto_contenedor");
+    //cada elemento de la pag
+    const $on_page_products = document.querySelectorAll(".producto_contenedor");
 
-  //agrega a cada elemento de la pag el boton y input
-  for (let product of $on_page_products) {
-    const $calc_price_button = document.createElement("p");
-    $calc_price_button.classList.add("boton-calcular");
-    $calc_price_button.setAttribute("style", calc_button_style);
-    $calc_price_button.textContent = "calcular";
+    //agrega a cada elemento de la pag el boton y input
+    for (let product of $on_page_products) {
+      const $calc_price_button = document.createElement("p");
+      $calc_price_button.classList.add("boton-calcular");
+      $calc_price_button.setAttribute("style", calc_button_style);
+      $calc_price_button.textContent = "calcular";
 
-    const $uni_amount_input = document.createElement("input");
-    $uni_amount_input.classList.add("cant--input");
-    $uni_amount_input.setAttribute("style", calc_input_style);
+      const $uni_amount_input = document.createElement("input");
+      $uni_amount_input.classList.add("cant--input");
+      $uni_amount_input.setAttribute("style", calc_input_style);
 
-    product.appendChild($calc_price_button);
-    product.appendChild($uni_amount_input);
+      product.appendChild($calc_price_button);
+      product.appendChild($uni_amount_input);
+    }
+
+    if ($on_page_products.length === 1) {
+      calc_product_price(null, $on_page_products[0]);
+    }
+
+    let next = "	";
+    let next_excel = "	";
+    //copiar precio o id al hacer click
+    //let xd = 1;
+
+    //al hacer click en el boton
+    addEventListener("click", (e) => {
+      if (e.target.classList.contains("boton-calcular"))
+        calc_product_price(e, null);
+
+      if (e.target.classList.contains("copiar")) {
+        let id = $box_product_id.textContent;
+        let name = $box_item_name.textContent;
+        let price_35 = $box_price_35.textContent;
+        let price_40_uni = $box_price_40_uni.textContent;
+        let units = $box_item_units.textContent;
+
+        let to_copy = `${id}${next}${name.trim()}${next}${units}${next}${price_35}${next}${price_40_uni}${next}-${next}-${next}-${next}-`;
+        navigator.clipboard.writeText(to_copy);
+      }
+
+      if (e.target.classList.contains("copiarind")) {
+        let id = $box_product_id.textContent;
+        let name = $box_item_name.textContent;
+        let price_40 = $box_price_40.textContent;
+        let price_40_uni = price_40;
+        let units = $box_item_units.textContent;
+
+        let to_copy = `${id}${next}${name.trim()}${next}${units}${next}${price_40}${next}${price_40_uni}${next}-${next}-${next}-${next}-`;
+        navigator.clipboard.writeText(to_copy);
+      }
+
+      if (e.target.classList.contains("cambiar-lado")) {
+        $box_container.classList.toggle("derecha");
+      }
+    });
   }
 
-  if ($on_page_products.length === 1) {
-    calc_product_price(null, $on_page_products[0]);
+  // let $box_item_name = document.getElementById("item");
+  //   let $box_item_name = document.getElementById("unidades");
+  //   let $box_cost_price = document.getElementById("precioCosto");
+  //   let $box_price_35 = document.getElementById("precio35");
+  //   let $box_price_40 = document.getElementById("precio40");
+  //   let $box_price_35_uni = document.getElementById("precio35Uni");
+  //   let $box_price_40_uni = document.getElementById("precio40Uni");
+  //   let $box_product_id = document.getElementById("codigo");
+
+  function closeIt() {
+    for (let product of $on_page_products) {
+      $calc_price_button = product.querySelector(".boton-calcular");
+      $uni_amount_input = product.querySelector(".cant--input");
+
+      product.removeChild($calc_price_button);
+      product.removeChild($uni_amount_input);
+    }
   }
-
-  let next = "	";
-  let next_excel = "	";
-  //copiar precio o id al hacer click
-  //let xd = 1;
-
-  //al hacer click en el boton
-  addEventListener("click", (e) => {
-    if (e.target.classList.contains("boton-calcular"))
-      calc_product_price(e, null);
-
-    if (e.target.classList.contains("copiar")) {
-      let id = $box_product_id.textContent;
-      let name = $box_item_name.textContent;
-      let price_35 = $box_price_35.textContent;
-      let price_40_uni = $box_price_40_uni.textContent;
-      let units = $box_item_units.textContent;
-
-      let to_copy = `${id}${next}${name.trim()}${next}${units}${next}${price_35}${next}${price_40_uni}`;
-      navigator.clipboard.writeText(to_copy);
-    }
-
-    if (e.target.classList.contains("copiarind")) {
-      let id = $box_product_id.textContent;
-      let name = $box_item_name.textContent;
-      let price_40 = $box_price_40.textContent;
-      let price_40_uni = price_40;
-      let units = $box_item_units.textContent;
-
-      let to_copy = `${id}${next}${name.trim()}${next}${units}${next}${price_40}${next}${price_40_uni}`;
-      navigator.clipboard.writeText(to_copy);
-    }
-
-    if (e.target.classList.contains("cambiar-lado")) {
-      $box_container.classList.toggle("derecha");
-    }
-  });
+  
+  window.onbeforeunload = closeIt;
+  Main();
+} else {
+  console.log("ya iniciado");
 }
-
-// let $box_item_name = document.getElementById("item");
-//   let $box_item_name = document.getElementById("unidades");
-//   let $box_cost_price = document.getElementById("precioCosto");
-//   let $box_price_35 = document.getElementById("precio35");
-//   let $box_price_40 = document.getElementById("precio40");
-//   let $box_price_35_uni = document.getElementById("precio35Uni");
-//   let $box_price_40_uni = document.getElementById("precio40Uni");
-//   let $box_product_id = document.getElementById("codigo");
-
-function closeIt() {
-  for (let product of $on_page_products) {
-    $calc_price_button = product.querySelector(".boton-calcular");
-    $uni_amount_input = product.querySelector(".cant--input");
-
-    product.removeChild($calc_price_button);
-    product.removeChild($uni_amount_input);
-  }
-}
-window.onbeforeunload = closeIt;
-
-Main();
