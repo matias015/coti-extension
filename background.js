9186a130fa4763ba683bc2b09e4c539e3e63cfc7
matias015 <matiasjf015@gmail.com>
2022-02-17 16:08:45 -0300

    v2.0

--- background.js
+++ background.js
@@ -1,3 +1,5 @@
-//chrome.browserAction.onclicked.addListener(function (tab) {
-//  chrome.tabs.executeScript(null, { file: "main.js" });
-//});
+chrome.tabs.onUpdated.addListener(function (tabId, info, tab){
+    if (info.status == "complete"){
+        chrome.tabs.executeScript(null, { file: "main.js" });
+    }
+})
--- main.js
+++ main.js
@@ -1,162 +1,442 @@
-// funciones y vars
 
-//definir variables para luego compleatar los campos en pricebox
-let item_name;
-let cost_price;
-let pric_35;
-let price_40;
-let price_35_uni;
-let price_40_uni;
-let product_id;
-let units;
+//check for local storage default vars
+let defaultTotal = localStorage.getItem('defaultTotal') || 1.4
+let defaultUnit = localStorage.getItem('defaultUnit') || 1.35
 
-//func para extrar datos del prod y calc precio final
-const calc_product_price = (e, element) => {
-  let selected_product;
-  if (element) {
-    selected_product = element.querySelector(".producto");
-  } else {
-    selected_product = e.target.parentElement.querySelector(".producto");
+function main(){
+  // global value to copy
+  let ToCopy = []
+
+  //values to calc seeall
+  const PorcValues = [1.25,1.3,1.35,1.4,1.45,1.5,1.55,1.6]
+
+  //style for html container inside each prod card
+  let style = `<style>.containter{width: 100%;height: 100%;background-color: aqua;}</style>`
+
+  let UI            // containt html element of ui
+  let FILTEREDVALUE //global value for input filter
+
+  let products = getProducts()
+
+  products.forEach( prod => {
+    let promo = inPromo(prod)
+    let price = getPrice(prod,promo)
+    let name = getName(prod)
+    let units = getUnits(name)
+
+    let cardInfo = find('div',prod)
+    cardInfo.removeChild(find('.content_bulto_unidad_kilo',cardInfo))
+
+    //calculate final prices with default values
+    let [final,finalUnit] = calculate(price,units)
+
+    // html to each card
+    let html = `
+    <hr style="width:90%;"/>
+      <input class="unitInput" id="amount" type="text" value="${units}">
+      <div style="width:100%" class="font flex">
+          <span class="font price" id="final">${final}</span> 
+          <span class="font price" id="units">${finalUnit}</span>
+      </div>
+      <p style="width: 75%;text-align:center;" class="pointer font see_all">ver todos</p>
+      <p style="width: 75%;text-align:center;" class="pointer font copy">copiar</p>
+      <input class="amountRef unitInput" type="text" value="1">
+
+      <style>
+      .price{ background-color:rgb(225,225,225); border-radius:5px; padding:0.25em}
+      .pointer{ cursor:pointer }
+      .font{ font-size: 1.3em; }
+      .see_all{ height:23px; width:75%; background-color: rgb(255, 14, 14); color:white; border-radius:5px; }
+      .copy{ width:75%; height:20px; outline: solid 3px rgb(255, 14, 14); border-radius:5px; }
+      .unitInput{width: 90%; border-radius:5px; text-align:center;}
+      .wh100{ width: 100%; height: 100%; }
+      .flex{ display: flex; justify-content: space-between; align-items: center; }
+      .mid{ width: 50%; height: 100%; }
+      </style>`
+
+    // create element to insert in card
+    let element = newEl('div',html+style,{
+      attrs:["class","wh100 flex","style","padding-top:1em;flex-direction:column;gap:1em;"]
+    })
+
+    // modified props of cards to avoid overflow and other css bugs
+    modifedProdCar(prod,promo)
+    prod.children[0].appendChild(element) 
+  })
+
+  UI = addUI()          // add input to default values in dom
+  setChangeEvent()      // to all inputs
+  setClickEvent()       // to seeall and copy buttons
+
+
+  function getProducts(){
+      return Array.from(findall(".producto"));
   }
 
-  //precio costo
-  cost_price = selected_product.querySelector(".producto_precio");
-  cost_price = cost_price.textContent.replaceAll("$", "");
-  cost_price = parseFloat(cost_price.trim());
-
-  //nombre producto
-  item_name = selected_product.querySelector(".producto_txt").textContent;
-
-  //conseguir codigo
-  product_id = selected_product.querySelector(".producto_id").textContent;
-
-  product_id = parseInt(product_id.replaceAll("Código: ", ""));
-
-  //conseguir el input de unidades
-  units = parseFloat(
-    selected_product.parentElement.querySelector(".cant--input").value
-  );
-
-  //si es 0 o esta vacio o hay una letra, sera 1
-  if (!units) units = 1;
-
-  //calcular precios al 35 y al 40 con las unidades
-  price_35 = Math.round(cost_price * 1.05 * 1.21 * 1.35 * 100) / 100;
-  price_35 = Math.round(price_35 * 4) / 4;
-
-  price_40 = Math.round(cost_price * 1.05 * 1.21 * 1.4 * 100) / 100;
-  price_40 = Math.round(price_40 * 4) / 4;
-
-  price_35_uni = Math.round((price_35 / units) * 100) / 100;
-  price_35_uni = Math.round(price_35_uni * 4) / 4;
-
-  price_40_uni = Math.round((price_40 / units) * 100) / 100;
-  price_40_uni = Math.round(price_40_uni * 4) / 4;
-
-  //mostrar los resultados en la box
-  $box_item_name.textContent = item_name;
-  $box_cost_price.textContent = cost_price;
-  $box_price_35.textContent = price_35;
-  $box_price_40.textContent = price_40;
-  $box_price_35_uni.textContent = price_35_uni;
-  $box_price_40_uni.textContent = price_40_uni;
-  $box_product_id.textContent = product_id;
-
-  bg_color_select(units);
-};
-
-const bg_color_select = (units) => {
-  //resaltar un precio u otro dependiendo de las unidades
-  if (units == 1) {
-    $box_price_35.style.backgroundColor = "transparent";
-    $box_price_40.style.backgroundColor = "black";
-    $box_price_35_uni.style.backgroundColor = "transparent";
-    $box_price_40_uni.style.backgroundColor = "transparent";
-  } else {
-    $box_price_35.style.backgroundColor = "black";
-    $box_price_40.style.backgroundColor = "transparent";
-    $box_price_35_uni.style.backgroundColor = "transparent";
-    $box_price_40_uni.style.backgroundColor = "black";
+  function inPromo(prod){
+    if(find('.con_promo',prod)) return true
+    else return false
   }
-};
 
-/////////////////////////////////////////////////////////////////////////////////////////////////////
+  function getPrice(prod,promo){
+    if(promo){
+      let price = find('.con_promo',prod).textContent
+      return price.replace('$','').trim()
+    }
+    else{
+      let price = find('.producto_precio',prod).textContent
+      return price.replace('$','').trim()
+  }}
 
-// MAIN
+  function getName(prod){
+      let name = find('.producto_txt',prod)
+      name.setAttribute('contenteditable','true')
+      return name.textContent.trim()
+  }
 
-//caja con precios
-let $show_prices_box = document.createElement("div");
-$show_prices_box.innerHTML = `
-<div style="font-weight:700;position:fixed;bottom:0;max-width:25vw;padding:1em;background-color:gray;font-size:1.5em;">
-    <p style="color:#000;" ><span id="item"></span></p></br>
-    <p style="color:#000;"><span>precio costo: $  </span><span class="value" id="precioCosto"></span></p>
-    <p style="color:lightgreen;"><span>.precio 35%: $</span><span class="value" id="precio35"></span></p>
-    <p style="color:lightgreen;"><span>.precio 40%: $</span><span class="value" id="precio40"></span></p>
-    <p style="color:lightblue;" ><span>unidad 35%: $</span><span class="value" id="precio35Uni"></span></p>
-    <p style="color:lightblue;" ><span>unidad 40%: $</span><span class="value" id="precio40Uni"></span></p>
-    <p style="color:#000;cursor:pointer;" ><span>codigo    :  </span><span class="value" id="codigo"></span></p>
-    <button class="copiar">copiar uni</button><button class="copiarind">copiar ind</button>
-    </div>`;
+  function getId(prod){
+      let id = find('.producto_id',prod).textContent
+      return id.replace('Código:','').trim()
+  }
 
-//agregar caja a dom
-document.body.appendChild($show_prices_box);
+  function getUnits(name){
+      let regexp = / x\d+/g;
+      let match = regexp.exec(name);
+      
+      if (match){
+          units = take(match,-1);
+          units = parseInt(units.replaceAll("x", "").trim());
+      }
+      else units = 1;
+      return units
+  }
 
-//objeto con elementos contenedores de los precios finales en la box
+  function calculate(price,units){
+    let val = round(price*defaultTotal,100)
+    
+    if(units === 1 || units === '1'){
+      return [`$ ${val}`, `$ ${val}`]
+    }
+    else {
+      return [`$ ${val}`, `$ ${round(price*defaultUnit/units,4)}`]
+    }
+  }
 
-let $box_item_name = document.getElementById("item");
-let $box_cost_price = document.getElementById("precioCosto");
-let $box_price_35 = document.getElementById("precio35");
-let $box_price_40 = document.getElementById("precio40");
-let $box_price_35_uni = document.getElementById("precio35Uni");
-let $box_price_40_uni = document.getElementById("precio40Uni");
-let $box_product_id = document.getElementById("codigo");
+  function setChangeEvent(){
+    window.addEventListener('change',unitChange)
+  }
 
-//estilo de boton y input
-let calc_button_style =
-  "cursor:pointer;border-radius:0.5em;text-align:center;width:60%;background-color:green;color:#fff;padding:0.5em;margin:1.5em auto 0.5em auto;";
+  function validateUnit(unit){
+    let regexp = /^([0-9]+)(,)?([0-9]+)?$/g
+    if(unit.match(regexp)) {
+      return unit.replaceAll(',','.')
+    }
+    else return 1
+  }
 
-let calc_input_style =
-  "border-radius:0.5em;text-align:center;width:90%;color:#000;padding:0.25em;margin:0 auto 2em auto;";
+  function unitChange(e){
+    // if input change is the default for total porcentage set the value on local storage
+    if(targetHas(e,'setDefault')){
+      let value = e.target.value
+      localStorage.setItem('defaultTotal',parseFloat(value))
+    }
+    // the same for unit input
+    else if(targetHas(e,'setDefaultUnit')){
+      let value = e.target  .value
+      localStorage.setItem('defaultUnit',parseFloat(value))
+    }
 
-//cada elemento de la pag
-const $on_page_products = document.querySelectorAll(".producto_contenedor");
+    //if its the input of some product
+    else if(targetHas(e,'unitInput')){
+      //get info
+      let prod = parent(e.target,2)
+      let price = getPrice(prod)
+      let newUnit = validateUnit(e.target.value)
+      // calc new prices
+      let [final,unit] = calculate(price,newUnit)
+      // set him in doc
+      find('#final',prod).textContent = final
+      find('#units',prod).textContent = unit
+    }
 
-//agrega a cada elemento de la pag el boton y input
-for (let product of $on_page_products) {
-  const $calc_price_button = document.createElement("p");
-  $calc_price_button.classList.add("boton-calcular");
-  $calc_price_button.setAttribute("style", calc_button_style);
-  $calc_price_button.textContent = "boton-calcular";
+    else if(targetHas(e,'filter')){
+      if(e.target.value == FILTEREDVALUE) return
+      else mark(e.target.value)
+    }
+  }
 
-  const $uni_amount_input = document.createElement("input");
-  $uni_amount_input.classList.add("cant--input");
-  $uni_amount_input.setAttribute("style", calc_input_style);
+  function search(name,searchVal){
+    let str = name.split('')
+    let value = searchVal.split('')
 
-  product.appendChild($calc_price_button);
-  product.appendChild($uni_amount_input);
+    let matches = 0
+
+    let i = 0
+
+    while(i<value.length){
+      let index = str.indexOf(value[i])
+      if(index!=-1){
+        if(value[i+1]==str[index+1]){
+          if(value[i+2]==str[index+2]){
+            matches+=4
+            str.splice(index,3)
+          }
+          else{
+            matches+=2
+            str.splice(index,2)
+          }
+        }
+        else{
+          matches+=0.5
+          str.splice(index,1)
+        }
+      }
+      else matches--
+      i++
+    }
+    return matches
+  }
+
+  function mark(val){
+    if(val == '') resetFilter()
+    FILTEREDVALUE = val
+    const exp =`[${val}]`
+    let names = Array.from(findall('.producto div .producto_txt'))
+    let matches= []
+    names.forEach((name,i) => {
+      matches.push([name,search(name.textContent,val)])
+    })
+    matches = matches.sort((a,b) => b[1]-a[1])
+    let max = matches[0][1]
+    let min = take(matches,-1)[1]
+    if(min<0) min = 0
+    matches.forEach(name=>{
+      let range = scale(name[1], min, max, 0, 255)
+      parent(name[0],2).style.backgroundColor = `rgb(${255-range},${range},0,0.5)`
+    })
+    
+    
+  }
+
+  function resetFilter(){
+    Array.from(findall('.producto')).forEach(el=>el.style.backgroundColor='')
+  }
+
+  function addUI(){
+    // contains the inputs for change defaults porc and seeall visualizer
+    const uiHtml = `
+    <span style="width: 15em;text-align:center;">filtrar</span><input style="width: 14em" class="filter"/>
+    <div class="seeallcontainer">
+      <div class="a1">
+        <p class="header">porc.</p>
+        <p>25% -> </p> <p>30% -> </p>
+        <p>35% -> </p> <p>40% -> </p>
+        <p>45% -> </p> <p>50% -> </p>
+        <p>55% -> </p> <p>60% -> </p>
+      </div>
+      <div class="a2">
+          <p class="header">total</p>
+          <p contenteditable class="showAllTotal showallnum"></p>
+          <p contenteditable class="showAllTotal showallnum"></p>
+          <p contenteditable class="showAllTotal showallnum"></p>
+          <p contenteditable class="showAllTotal showallnum"></p>
+          <p contenteditable class="showAllTotal showallnum"></p>
+          <p contenteditable class="showAllTotal showallnum"></p>
+          <p contenteditable class="showAllTotal showallnum"></p>
+          <p contenteditable class="showAllTotal showallnum"></p>
+      </div>
+      <div class="a3">
+          <p contenteditable class="header">unidah</p>
+          <p contenteditable class="showAllUnit showallnum"></p>
+          <p contenteditable class="showAllUnit showallnum"></p>
+          <p contenteditable class="showAllUnit showallnum"></p>
+          <p contenteditable class="showAllUnit showallnum"></p>
+          <p contenteditable class="showAllUnit showallnum"></p>
+          <p contenteditable class="showAllUnit showallnum"></p>
+          <p contenteditable class="showAllUnit showallnum"></p>
+          <p contenteditable class="showAllUnit showallnum"></p>
+      </div>
+      <div class="swiper pointer"><</div>
+    </div>
+
+
+    <style>
+      .swiper{ background-color:gray; border-radius:3px; width:20px; text-align:center; height:100px;line-height:100px;}
+      .swiped{ transform: translateX(-21em) }
+      .header{ padding-bottom:1em; }
+      .seeallcontainer{ font-size:1.25em; font-weight:600; display: flex; align-items:center;width: 100%; height: 100%; }
+      .a1{ grid-column:0/1; padding: 1em}
+      .a2{ grid-column:1/2; padding: 1em}
+      .a3{ grid-column:2/3; padding: 1em}
+      .showallnum{ cursor:pointer }
+      .showallnum:hover{ color:red; }
+      </style>
+    <input value="${defaultTotal}" style="width: 8em" class="setDefault" type="text" >
+    <input value="${defaultUnit}" type="text" style="width: 8em" class="setDefaultUnit">`
+
+    newEl('div',uiHtml,{
+      parent:document.body,
+      attrs:['class','ui','style','background-color:lightgray;position: fixed;bottom: 0;left: 0;padding:1em;']
+    })
+    return find('.ui')
+  }
+
+  function copy(){
+    cbcopy(ToCopy.join('\t'))
+  }
+
+  function getCopyInfo(element){
+    ToCopy = []
+
+    let priceTotal = find('#final',element).textContent
+    let priceUnit = find('#units',element).textContent
+    let card = parent(element)
+
+    let name = getName(card)
+    let id = getId(card)
+    let units = find('.unitInput',card).value
+    let amounref = find('.amountRef',card).value
+
+    ToCopy = pushAll(ToCopy,id,name,units,priceTotal,priceUnit,amounref)
+    copy()
+  }
+
+  function setClickEvent(){
+    window.addEventListener('click', e => {
+      
+      if(targetHas(e,'see_all')){
+        let prod = parent(e.target,2)
+        let price = getPrice(prod)
+        seeAllCalc(prod,price)
+        UI.classList.remove('swiped')
+      }
+
+      if(targetHas(e,'copy')){
+        getCopyInfo(parent(e.target))
+      }
+
+      if(targetHas(e,'swiper')){
+        UI.classList.toggle('swiped')
+      }
+
+      if(targetHas(e,'showAllTotal')){
+        ToCopy[3] = `$ ${e.target.textContent}`
+        copy()
+      }
+
+      if(targetHas(e,'showAllUnit')){
+        ToCopy[4] = `$ ${e.target.textContent}`
+        copy()
+      }
+
+    })
+  }
+
+  function modifedProdCar(prod,promo){
+    prod.style.height = '650px'
+    if(!promo) prod.style.overflowX = 'hidden'
+    prod.style.marginBottom = '50px'
+  }
+
+  function seeAllCalc(prod,price){
+    let totalPrices = []
+    let unitPrices = []
+
+    // get units from card input
+    let units = find('.unitInput',prod).value
+
+    for(let value of PorcValues){
+      let total = round(price*value,10)
+      let unit = round(price*value/units,4)
+
+      totalPrices.push(total)
+      unitPrices.push(unit)
+    }
+    seeallSetValues(totalPrices,unitPrices,prod)
+  }
+
+  function seeallSetValues(totalPrices,unitPrices){
+    let totalElements = findall('.showAllTotal')
+    let unitElements = findall('.showAllUnit')
+
+    fillContent(totalElements,totalPrices)
+    fillContent(unitElements,unitPrices)
+  }
+
+
+
+
+  ////////////////////////////////////////////////////////////////////
+  function find(id, parent) {
+    if (parent) return parent.querySelector(id)
+    else return document.querySelector(id)
+  }
+
+  function findall(id, parent) {
+    if (parent) return parent.querySelectorAll(id)
+    else return document.querySelectorAll(id)
+  }
+
+  function newEl(tag, cont, props) {
+      if(!props) props = {}
+    const e = document.createElement(tag)
+    e.innerHTML = cont
+    if (props.clss) e.classList.add(props.clss)
+    if (props.attrs) {
+      for (let i = 0; i < props.attrs.length;) {
+        e.setAttribute(props.attrs[i], props.attrs[i + 1])
+        i = i + 2
+      }
+    }
+    if (props.parent) props.parent.appendChild(e)
+    else return e
+  }
+  
+  function round(val,div){
+    return Math.round(val * div) / div;
+  }
+
+  function parent(el, deep) {
+    if (!deep) deep = 1
+    while (deep > 0) {
+      el = el.parentElement
+      if (!el) console.log('cant find parent')
+      deep--
+    }
+    return el
+  }
+
+  function targetHas(e, clss) {
+    if (e.target.classList.contains(clss)) return true
+    else return false
+  }
+
+  function fillContent (elements,content){
+    for(let i in elements){
+      elements[i].textContent = content[i]
+    }
+  };
+
+  function take(arr,i){
+    if(i<0) i = arr.length + i
+    return arr[i]
+  }
+  
+  function pushAll(arr,...el){
+    for(let e of el){
+      arr.push(e)
+    }
+    return arr
+  }
+
+  function cbcopy(text) {
+    navigator.clipboard.writeText(text)
+      .catch(() => console.log('there was a problem'))
+  }
+
+  function scale (number, inMin, inMax, outMin, outMax) {
+    return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+  }
 }
 
-if ($on_page_products.length === 1) {
-  calc_product_price(null, $on_page_products[0]);
-}
-
-let next = "	";
-let next_excel = "	";
-//copiar precio o id al hacer click
-//let xd = 1;
-
-//al hacer click en el boton
-addEventListener("click", (e) => {
-  if (e.target.classList.contains("boton-calcular"))
-    calc_product_price(e, null);
-
-  if (e.target.classList.contains("copiar")) {
-    let to_copy = `${product_id}${next}${item_name.trim()}${next}-${next}${price_35}${next}${price_40_uni}`;
-    navigator.clipboard.writeText(to_copy);
-  }
-
-  if (e.target.classList.contains("copiarind")) {
-    let to_copy = `${product_id}${next}${item_name.trim()}${next}-${next}${price_40}${next}${price_40}`;
-    navigator.clipboard.writeText(to_copy);
-  }
-});
+if(!document.querySelector('.ui')) main()
